67d96a06d673f590a703e641e9c64fcd6c50064c
anfaas <ishad0911@gmail.com>
2020-06-23 01:52:44 +0530

    error in getting spaces fix it asap

--- src/main/java/rock.java
+++ src/main/java/rock.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.safari.SafariDriver;
 
 import org.openqa.selenium.safari.SafariOptions;
@@ -12,6 +13,8 @@ import java.util.Random;
 
 import java.util.Scanner;
 import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.compile;
 
 public class rock
 
@@ -30,13 +33,28 @@ public class rock
         String htmlData =driver.getPageSource();
         String[] firstkey_split=htmlData.split("\"TextInput-item TextInput-item--cursor\">");
         String[] firstkey=firstkey_split[1].split("</span>");
+        list.add(firstkey[0]);
+    //  System.out.println(htmlData);
+         final Pattern
+                rx1 = Pattern.compile("class=\"TextInput-item\">(.*?)</span"),
+                rx2 = Pattern.compile("TextInput-item--special\">(.*?)</");
+        Matcher      m1 = rx1.matcher(htmlData),m2=rx2.matcher(htmlData);
 
-        System.out.println(firstkey[0]);
-//        Pattern   p = Pattern.compile("<img src=\"(.*?)\" alt");
-//        Matcher      m = p.matcher(htmlData);
-        driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div/div[3]").click();
+                        while (m1.find() ) {
+                            System.out.println(m1.group(1));
+                               m2.find();
+                               System.out.println(m2.group(1));
 
-
+//                    list.add(m.group(1));
+                }
+//        driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div/div[3]").click();
+//        driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div")
+//                .sendKeys(firstkey[0]);
+//                        for (String letter :list)
+//
+//        driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div")
+//                .sendKeys(letter);
 
     }
+
 }
