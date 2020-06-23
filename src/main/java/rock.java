b50b2aaa93000e46688d0762cfb9ff89faf3faf0
anfaas1618 <ishad0911@gmail.com>
2020-06-23 16:07:14 +0530

    made it extract the letters . bug in key click

--- src/main/java/rock.java
+++ src/main/java/rock.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import org.openqa.selenium.safari.SafariOptions;
@@ -24,9 +25,11 @@ public class rock
 
         List<String> list=new ArrayList<String>();
 
-        SafariOptions options = new SafariOptions();
-
-        SafariDriver driver = new SafariDriver(options);
+//        SafariOptions options = new SafariOptions();
+//
+//        SafariDriver driver = new SafariDriver(options);
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\DRAKH\\OneDrive\\Desktop\\geckodriver.exe");
+        FirefoxDriver driver =new FirefoxDriver();
 
         driver.get("https://www.keybr.com");
         driver.findElementByXPath("/html/body/div[2]/div/div[2]/div/a").click();
@@ -34,26 +37,31 @@ public class rock
         String[] firstkey_split=htmlData.split("\"TextInput-item TextInput-item--cursor\">");
         String[] firstkey=firstkey_split[1].split("</span>");
         list.add(firstkey[0]);
-    //  System.out.println(htmlData);
+     // System.out.println(htmlData);
          final Pattern
-                rx1 = Pattern.compile("class=\"TextInput-item\">(.*?)</span"),
+                rx1 = Pattern.compile("(?<=<span class=\".{0,70} ?\">).(?=<\\/span>)"),
                 rx2 = Pattern.compile("TextInput-item--special\">(.*?)</");
-        Matcher      m1 = rx1.matcher(htmlData),m2=rx2.matcher(htmlData);
-
+                Matcher  m1 = rx1.matcher(htmlData),
+                        m2=rx2.matcher(htmlData);
+int x=0;
                         while (m1.find() ) {
-                            System.out.println(m1.group(1));
-                               m2.find();
-                               System.out.println(m2.group(1));
-
-//                    list.add(m.group(1));
+                                 System.out.println(m1.group());
+                            ++x;
+                                 if (x>4)
+                                     list.add(m1.group());
                 }
-//        driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div/div[3]").click();
+                        for (String s :list)
+                            System.out.println("here here "+s);
+        driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div/div[3]").click();
 //        driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div")
 //                .sendKeys(firstkey[0]);
-//                        for (String letter :list)
-//
-//        driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div")
-//                .sendKeys(letter);
+                        for (String letter :list)
+if (letter.contains("␣"))
+        driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div")
+                .sendKeys(Keys.SPACE);
+else
+    driver.findElementByXPath("//*[@id=\"root\"]/section/div[3]/div")
+            .sendKeys(letter);
 
     }
 
